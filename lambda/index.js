63517c9d28a836b65219f5aeb4dcd76c7c170e98
RayeEThompson <rachael.elizabeth.thompson@gmail.com>
2021-03-29 13:08:13 -0500

    fix data return 3

--- lambda/index.js
+++ lambda/index.js
@@ -70,9 +70,10 @@ const FeaturedProductIntentHandler = {
   async handle(handlerInput) {
     const response = await httpGet();
     console.log(response);
+    console.log(response.data[0].name);
 
     return handlerInput.responseBuilder
-      .speak(`Okay, here is what I got for you ${response.data.name}`)
+      .speak(`Okay, here is what I got for you ${response.data[0].name}`)
       .reprompt("Would you like to hear the featured product?")
       .getResponse();
   },
